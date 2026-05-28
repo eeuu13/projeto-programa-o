cbcca1403773e24ead99c1252fad0a7c6f1aff7d
eeuu13 <email.24849@gmail.com>
2026-05-28 16:02:21 +0100

    Add files via upload

---
+++ SVGElements.cpp
@@ -0,0 +1,60 @@
+#include "SVGElements.hpp"
+
+namespace svg
+{
+    // These must be defined!
+    SVGElement::SVGElement() {}
+    SVGElement::~SVGElement() {}
+
+    // Ellipse (initial code provided)
+    Ellipse::Ellipse(const Color &fill,
+                     const Point &center,
+                     const Point &radius)
+        : fill(fill), center(center), radius(radius)
+    {
+    }
+    void Ellipse::draw(PNGImage &img) const
+    {
+        img.draw_ellipse(center, radius, fill);
+    }
+
+    // @todo provide the implementation of SVGElement derived classes
+    // HERE -->
+    void Circle::Circle(const Color &fill,
+                     const Point &center,
+                     const Point &radius)
+        : Ellipse(fill,center,{radius,radius})
+
+    Polyline::Polyline(const Color &stroke,
+                     const Point* &points)
+        : stroke(stroke), points(points)
+    {
+    }
+    void Polyline::draw(PNGImage &img) const
+    {
+        for (unsigned long i=0;i<points.size()-1;i++){
+            img.draw_line(points[i],points[i+1],stroke);
+        }
+    }
+    Line::Line(const Color &stroke,
+            const Point[2] &points)
+        : Polyline(stroke,points)
+    { 
+    }
+    Polygon::Polygon(const Color &fill,
+                     const vector<Point> &points)
+        : fill(fill), points(points)
+    {
+    }
+    void Polygon::draw(PNGImage &img) const
+    {
+        img.draw_polygon(points,fill);
+    }
+    Rectangle::Rectangle(const Color &fill,
+                        const vector<Point> &points)
+        : Polygon(fill,points)
+    {
+    }
+    
+
+}
---
+++ SVGElements.hpp
@@ -0,0 +1,81 @@
+//! @file shape.hpp
+#ifndef __svg_SVGElements_hpp__
+#define __svg_SVGElements_hpp__
+
+#include "Color.hpp"
+#include "Point.hpp"
+#include "PNGImage.hpp"
+
+namespace svg
+{
+    class SVGElement
+    {
+
+    public:
+        SVGElement();
+        virtual ~SVGElement();
+        virtual void draw(PNGImage &img) const = 0;
+    };
+
+    // Declaration of namespace functions
+    // readSVG -> implement it in readSVG.cpp
+    // convert -> already given (DO NOT CHANGE) in convert.cpp
+
+    void readSVG(const std::string &svg_file,
+                 Point &dimensions,
+                 std::vector<SVGElement *> &svg_elements);
+    void convert(const std::string &svg_file,
+                 const std::string &png_file);
+
+    class Ellipse : public SVGElement
+    {
+    public:
+        Ellipse(const Color &fill, const Point &center, const Point &radius);
+        void draw(PNGImage &img) const override;
+
+    private:
+        Color fill;
+        Point center;
+        Point radius;
+    };
+
+    class Circle : public Ellipse
+    {
+    public:
+        Circle(const Color &fill, const Point &center, const int &radius);
+    }
+    
+    class Polyline: public SVGElement
+    {
+    public:
+        Polyline(const Color &stroke,const Point* points);
+        void draw(PNGImage &img) const override;
+    private:
+        Color stroke;
+        Point* points;
+    }
+
+    class Line : public Polyline{
+    public:
+        Line(const Color &stroke,const Point[2] points);
+
+    }
+
+    class Polygon : public SVGElement
+    {
+    public:
+        Polygon(const Color &fill,const vector<Point> points);
+        void  draw (PNGImage &img) const override;
+    private:
+        Color fill;
+        Point* points;
+    }
+
+    class Rectangle : public Polygon
+    {
+    public:
+        Rectangle(const Color &fill,const vector<Point> points);
+    }
+    
+}
+#endif
