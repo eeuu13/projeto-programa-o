4732feac0b0b2aa46f8914bb549064cc28b38829
eeuu13 <email.24849@gmail.com>
2026-06-01 16:05:29 +0100

    Update readSVG.cpp

--- readSVG.cpp
+++ readSVG.cpp
@@ -23,6 +23,94 @@ namespace svg
         }
         return points;
     }
+    void a_child(XMLElement *element, vector<SVGElement *>& svg_elements)
+        {
+            for (XMLElement *child = element->FirstChildElement(); child != nullptr; child = child->NextSiblingElement())
+            {
+                if ((string)child->Name() == "ellipse")
+                {
+                    Color fill;
+                    Point center,radius;
+                    for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                    {
+                        if ((string)attr->Name() == "fill") fill = parse_color(attr->Value());
+                        else if ((string)attr->Name() == "cx") center.x = attr->IntValue();
+                        else if ((string)attr->Name() == "cy") center.y = attr->IntValue();
+                        else if ((string)attr->Name() == "rx") radius.x = attr->IntValue();
+                        else if ((string)attr->Name() == "ry") radius.y = attr->IntValue();
+                    }
+                    svg_elements.push_back(new Ellipse(fill, center, radius));
+                }
+                else if ((string)child->Name() == "circle")
+                {
+                    Color fill;
+                    Point center;
+                    int radius;
+                    for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                    {
+                        if ((string)attr->Name() == "fill") fill = parse_color(attr->Value());
+                        else if ((string)attr->Name() == "cx") center.x = attr->IntValue();
+                        else if ((string)attr->Name() == "cy") center.y = attr->IntValue();
+                        else if ((string)attr->Name() == "r") radius = attr->IntValue();
+                    }
+                    svg_elements.push_back(new Circle(fill, center, radius));
+                }
+                else if ((string)child->Name() == "polyline")
+                {
+                    Color stroke;
+                    vector<Point> points;
+                    for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                    {
+                        if ((string)attr->Name() == "stroke") stroke = parse_color(attr->Value());
+                        else if ((string)attr->Name() == "points") points = parse_points(attr->Value());
+                    } 
+                    svg_elements.push_back(new Polyline(stroke, points));
+                }
+                else if ((string) child->Name() == "line")
+                {
+                    Color stroke;
+                    Point point1, point2;
+                    for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                    {
+                        if ((string) attr->Name() == "stroke") stroke = parse_color(attr->Value());
+                        else if ((string) attr->Name() == "x1") point1.x = attr->IntValue();
+                        else if ((string) attr->Name() == "x2") point2.x = attr->IntValue();
+                        else if ((string) attr->Name() == "y1") point1.y = attr->IntValue();
+                        else if ((string) attr->Name() == "y2") point2.y = attr->IntValue();
+                    } 
+                    svg_elements.push_back(new Line(stroke, vector<Point>{point1, point2}));
+                }
+                else if ((string)child->Name() == "polygon")
+                {
+                    Color fill;
+                    vector<Point> points;
+                    for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                    {
+                        if ((string)attr->Name() == "fill") fill = parse_color(attr->Value());
+                        else if ((string)attr->Name() == "points") points = parse_points(attr->Value());
+                    } 
+                    svg_elements.push_back(new Polygon(fill, points));
+                }
+                else if ((string) child->Name() == "rect")
+                {
+                    Color fill;
+                    Point origin, size;
+                    for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
+                    {
+                        if ((string) attr->Name() == "fill") fill = parse_color(attr->Value());
+                        else if ((string) attr->Name() == "x") origin.x = attr->IntValue();
+                        else if ((string) attr->Name() == "y") origin.y = attr->IntValue();
+                        else if ((string) attr->Name() == "width") size.x = attr->IntValue();
+                        else if ((string) attr->Name() == "height") size.y = attr->IntValue();
+                    } 
+                    svg_elements.push_back(new Rectangle(fill, origin, size));
+                }
+                else if (string((string)child->Name()) == "g")
+                {
+                    a_child(child, svg_elements);
+                }
+            } 
+        }
     void readSVG(const string& svg_file, Point& dimensions, vector<SVGElement *>& svg_elements)
     {
         XMLDocument doc;
@@ -35,82 +123,6 @@ namespace svg
 
         dimensions.x = xml_elem->IntAttribute("width");
         dimensions.y = xml_elem->IntAttribute("height");
-        
-        // TODO complete code -->
-        for (XMLElement *child = xml_elem->FirstChildElement(); child != nullptr; child = child->NextSiblingElement())
-        {
-            if (child->Name() == "ellipse")
-            {
-                Color fill;
-                Point center,radius;
-                for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
-                {
-                    if (attr->Name() == "fill") fill = parse_color(attr->Value());
-                    else if (attr->Name() == "cx") center.x = attr->IntValue();
-                    else if (attr->Name() == "cy") center.y = attr->IntValue();
-                    else if (attr->Name() == "rx") radius.x = attr->IntValue();
-                    else if (attr->Name() == "ry") radius.y = attr->IntValue();
-                }
-                svg_elements.push_back(new Ellipse(fill, center, radius));
-            }
-            else if (child->Name() == "circle")
-            {
-                Color fill;
-                Point center;
-                int radius;
-                for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
-                {
-                    if (attr->Name() == "fill") fill = parse_color(attr->Value());
-                    else if (attr->Name() == "cx") center.x = attr->IntValue();
-                    else if (attr->Name() == "cy") center.y = attr->IntValue();
-                    else if (attr->Name() == "r") radius = attr->IntValue();
-                }
-                svg_elements.push_back(new Circle(fill, center, radius));
-            }
-            else if (child->Name() == "polyline")
-            {
-                Color stroke;
-                vector<Point> points;
-                for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
-                {
-                    if (attr->Name() == "stroke") stroke = parse_color(attr->Value());
-                    else if (attr->Name() == "points") points = parse_points(attr->Value());
-                } 
-                svg_elements.push_back(new Polyline(stroke, points));
-            }
-            else if (child->Name() == "line")
-            {
-                Color stroke;
-                vector<Point> points;
-                for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
-                {
-                    if (attr->Name() == "stroke") stroke = parse_color(attr->Value());
-                    else if (attr->Name() == "points") points = parse_points(attr->Value());
-                } 
-                svg_elements.push_back(new Line(stroke, points));
-            }
-            else if (child->Name() == "polygon")
-            {
-                Color fill;
-                vector<Point> points;
-                for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
-                {
-                    if (attr->Name() == "fill") fill = parse_color(attr->Value());
-                    else if (attr->Name() == "points") points = parse_points(attr->Value());
-                } 
-                svg_elements.push_back(new Polygon(fill, points));
-            }
-            else if (child->Name() == "rect")
-            {
-                Color fill;
-                vector<Point> points;
-                for (const XMLAttribute *attr = child->FirstAttribute(); attr != nullptr; attr = attr->Next())
-                {
-                    if (attr->Name() == "fill") fill = parse_color(attr->Value());
-                    else if (attr->Name() == "points") points = parse_points(attr->Value());
-                } 
-                svg_elements.push_back(new Rectangle(fill, points));
-            }
-        }   
+        a_child(xml_elem,svg_elements);
     }
 }
