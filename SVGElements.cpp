ed77a63bf73cc0c9f6495be9a39bb1c23ec42d7c
eeuu13 <email.24849@gmail.com>
2026-06-01 15:06:48 +0100

    Update SVGElements.cpp

--- SVGElements.cpp
+++ SVGElements.cpp
@@ -17,6 +17,20 @@ namespace svg
     {
         img.draw_ellipse(center, radius, fill);
     }
+    void Ellipse::translate(const int &x,const int &y){
+        center=center.translate({x,y});
+    }
+    void Ellipse::rotate(const int &v){
+        Point transform_origin={0,0};//TO DO: change into the input
+        center=center.rotate(transform_origin,v);
+    }
+    void Ellipse::scale(const int &v){
+        Point transform_origin={0,0};//TO DO: change into the input
+        radius.y*=v;
+        radius.x*=v;
+        center=center.scale(transform_origin,v);
+
+    }
 
     // @todo provide the implementation of SVGElement derived classes
     // HERE -->
@@ -38,6 +52,26 @@ namespace svg
             img.draw_line(points[i],points[i+1],stroke);
         }
     }
+    void Polyline::translate(const int &x, const int &y){
+        for (unsigned long i=0;i<points.size();i++){
+            points[i]=points[i].translate({x,y});
+        }
+    }
+
+    void Polyline::rotate(const int &v){
+        Point transform_origin={0,0};//TO DO: change into the input
+        for (unsigned long i=0;i<points.size();i++){
+            points[i]=points[i].rotate(transform_origin,v);
+        }
+    }
+
+    void Polyline::scale(const int &v){
+        Point transform_origin={0,0};//TO DO: change into the input
+        for (unsigned long i=0;i<points.size();i++){
+            points[i]=points[i].scale(transform_origin,v);
+        }
+    }
+
     Line::Line(const Color &stroke,
             const vector<Point> &points)
         : Polyline(stroke,points)
@@ -52,6 +86,27 @@ namespace svg
     {
         img.draw_polygon(points,fill);
     }
+
+    void Polygon::translate(const int &x, const int &y){
+        for (unsigned long i=0;i<points.size();i++){
+            points[i]=points[i].translate({x,y});
+        }
+    }
+
+    void Polygon::rotate(const int &v){
+        Point transform_origin={0,0};//TO DO: change into the input
+        for (unsigned long i=0;i<points.size();i++){
+            points[i]=points[i].rotate(transform_origin,v);
+        }
+    }
+
+    void Polygon::scale(const int &v){
+        Point transform_origin={0,0};//TO DO: change into the input
+        for (unsigned long i=0;i<points.size();i++){
+            points[i]=points[i].scale(transform_origin,v);
+        }
+    }
+
     Rectangle::Rectangle(const Color &fill,
                         const vector<Point> &points)
         : Polygon(fill,points)
